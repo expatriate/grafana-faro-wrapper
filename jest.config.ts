91a552930654134f15655a08274185153426f04d
Dmitry <expatriate@inbox.ru>
2026-09-25 17:57:40 +0300

    Compiled tests as CommonJS in ts-jest to silence TS151002 without isolatedModules

--- jest.config.ts
+++ jest.config.ts
@@ -1,9 +1,11 @@
 import type { Config } from 'jest';
 
 const config: Config = {
-  preset: 'ts-jest',
   testEnvironment: 'node',
   roots: ['<rootDir>/src'],
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: { module: 'commonjs', moduleResolution: 'node10' } }],
+  },
 };
 
 export default config;
